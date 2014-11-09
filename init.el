9352ec0eb7a132744ed60605db09f24d9ca70181
Brian Jou <JouBrian@gmail.com>
2014-11-08 16:38:26 -0800

    updated emacs config

--- init.el
+++ init.el
@@ -1,19 +1,40 @@
+; normalize
 (delete-selection-mode t)
 (add-hook 'before-save-hook '(lambda nil (untabify 0 (point-max))))
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
-(add-hook 'html-mode-hook
-          (lambda()
-            (setq sgml-basic-offset 2)
-            (setq indent-tabs-mode t)))
 (setq require-final-newline t)
 (global-font-lock-mode 1)
 (setq make-backup-files nil)
 (prefer-coding-system 'utf-8)
 (setq coding-system-for-read 'utf-8)
 (setq coding-system-for-write 'utf-8)
+(menu-bar-mode 0)
+;; use spaces for tabs
+(setq-default indent-tabs-mode nil)
+(setq inhibit-default-init t)
+
+;sfang
+
+;; Do not use the iconifiy shortcut
+;(global-unset-key [C-z] nil)
+
+;; Need to be able to jump around
+(global-set-key [M-g] 'goto-line)
+
+;; Make control-tab switch windows
+;(global-set-key [C-tab] (lambda () (interactive) (other-window 1)))
+;(global-set-key (kbd "<C-S-iso-lefttab>") (lambda () (interactive) (other-window -1)))
+
+;end sfang
+
+; html mode
+(add-hook 'html-mode-hook
+          (lambda()
+            (setq sgml-basic-offset 2)
+            (setq indent-tabs-mode t)))
 
 ; directory to put various el files into
-(add-to-list 'load-path "~/.emacs.d")
+(add-to-list 'load-path "~/.emacs.d/legacy")
 
 ; obj-c mode
 (autoload 'objc-mode' "cc-mode" "Objective-C Editing Mode" t)
@@ -25,12 +46,9 @@
                            (setq c-basic-offset 4)
                            (setq indent-tabs-mode nil)))
 ; ruby mode
-(autoload 'ruby-mode "ruby-mode" "Major mode for editing ruby scripts." t)
-(setq auto-mode-alist  (cons '(".rb$" . ruby-mode) auto-mode-alist))
-(setq ruby-insert-encoding-magic-comment nil)
-(add-hook 'ruby-mode-hook
-          (require 'ruby-electric)
-          (ruby-electric-mode t))
+(add-to-list 'load-path "~/.emacs.d/legacy/Enhanced-Ruby-Mode/")
+(require 'enh-ruby-mode)
+
 ; js mode
 (autoload 'javascript-mode "javascript-mode" "JavaScript mode" t)
 (setq auto-mode-alist (append '(("\\.js$" . javascript-mode))
@@ -38,6 +56,30 @@ auto-mode-alist))
 (setq auto-mode-alist (append '(("\\.erb$" . javascript-mode))
 auto-mode-alist))
 (setq js-indent-level 2)
+; py mode
+(add-hook 'python-mode-hook '(lambda ()
+                               (setq python-indent 2)))
+(defadvice python-calculate-indentation (around outdent-closing-brackets)
+  "Handle lines beginning with a closing bracket and indent them so that
+they line up with the line containing the corresponding opening bracket."
+  (save-excursion
+    (beginning-of-line)
+    (let ((syntax (syntax-ppss)))
+      (if (and (not (eq 'string (syntax-ppss-context syntax)))
+               (python-continuation-line-p)
+               (cadr syntax)
+               (skip-syntax-forward "-")
+               (looking-at "\\s)"))
+          (progn
+            (forward-char 1)
+            (ignore-errors (backward-sexp))
+            (setq ad-return-value (current-indentation)))
+        ad-do-it))))
+
+(ad-activate 'python-calculate-indentation)
+; scala mode
+(add-to-list 'load-path "~/.emacs.d/legacy/scala-mode2/")
+(require 'scala-mode2)
 
 ;; Shift the selected region right if distance is postive, left if
 ;; negative
